604fa293929c054f45a7e251cdf2284c1819a1bf
SergiOn <Onishhenko@gmail.com>
2017-01-19 23:48:31 +0200

    02-simple-1

---
+++ home.js
@@ -0,0 +1,5 @@
+'use strict';
+
+let welcome = require('./welkome');
+
+welcome('home');
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    entry: "./home.js",
+    output: {
+        filename: "build.js"
+    }
+};
