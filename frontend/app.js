c38d9295f6c8363f02622a4d18669469601be080
SergiOn <Onishhenko@gmail.com>
2017-02-03 01:04:21 +0200

    04-require-4 # server works with context without stuff.js file

--- frontend/app.js
+++ frontend/app.js
@@ -4,5 +4,5 @@ let moduleName = location.pathname.slice(1);
 
 let context = require.context('./routes', false);
 
-let route = require('./routes/' + moduleName);
+let route = context('./' + moduleName);
 route();
