edb82b37ac395b09306ea572a420eba9713a57d7
SergiOn <Onishhenko@gmail.com>
2017-02-04 21:23:21 +0200

    04-require-5 # with context

--- frontend/app.js
+++ frontend/app.js
@@ -4,7 +4,8 @@ let moduleName = location.pathname.slice(1);
 
 let handler;
 try {
-    handler = require('bundle!./routes/' + moduleName);
+    let context = require.context('bundle!./routes/', true, /^\.\//);
+    handler = context('./' + moduleName);
 } catch (e) {
     alert('No such path');
 }
