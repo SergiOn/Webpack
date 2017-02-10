570c0d1d473b52d95323a222ed449e25a5c3ece9
SergiOn <Onishhenko@gmail.com>
2017-02-10 14:35:19 +0200

    05-library-1 # require lodash but it don't work in me

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,18 @@
 'use strict';
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
-const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    context: path.resolve(__dirname, 'frontend'),
-
-    entry: {
-        app: "./app"
-    },
+    context: './frontend',
+    entry: "./app",
 
     output: {
-        path: __dirname + '/public/js',
-        publicPath: "/js/",
-        filename: "[name].js"
+        path: __dirname + '/public',
+        publicPath: "/",
+        filename: "app.js"
     },
 
-    watch: NODE_ENV == 'development',
-
-    watchOptions: {
-        aggregateTimeout: 100
-    },
-
-    devtool: NODE_ENV == 'development' ? "cheap-inline-module-source-map" : false,
-
-
-    resolve: {
-        modulesDirectories: ['node_modules'],
-        extensions: ['', '.js']
-    },
-
-    resolveLoader: {
-        modulesDirectories: ['node_modules'],
-        moduleTemplates: ['*-loader', '*'],
-        extensions: ['', '.js']
-    },
-
-    module: {
-
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel',
-            query: {
-                presets: ['es2015']
-            }
-        }, {
-            test: /\.html$/,
-            loader: 'raw'
-        }]
+    externals: {
+        lodash: '_'
     }
 };
-
-if (NODE_ENV == 'production') {
-    module.exports.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-                drop_console: true,
-                unsafe: true
-            }
-        })
-    );
-}
