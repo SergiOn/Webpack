19ea4c7dc804b50d746c7853e7d760e6c9c37269
SergiOn <Onishhenko@gmail.com>
2017-02-27 22:45:00 +0200

    06-style-assets-3 # url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,7 @@ module.exports = {
             loader: "style!css!autoprefixer?browsers=last 2 version!"
         }, {
             test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
-            include: /node_modules/,
-            loader: "file?name=[1].[ext]&regExp=node_modules\\\\(.*)"
-        }, {
-            test: /\.(png|jpe?g|svg|ttf|eot|woff|woff2)$/,
-            exclude: /node_modules/,
-            loader: "file?name=[path][name].[ext]"
+            loader: "url?name=[path][name].[ext]&limit=4096"
         }]
     }
 };
