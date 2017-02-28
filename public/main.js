48ac4076f054654f9fc15199619c263f54e67d75
SergiOn <Onishhenko@gmail.com>
2017-02-28 19:25:03 +0200

    08-live-reload-1 # use webpack-dev-server

--- public/main.js
+++ public/main.js
@@ -1,8 +1,7 @@
-var common =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// install a JSONP callback for chunk loading
-/******/ 	var parentJsonpFunction = window["webpackJsonp_name_"];
-/******/ 	window["webpackJsonp_name_"] = function webpackJsonpCallback(chunkIds, moreModules) {
+/******/ 	var parentJsonpFunction = window["webpackJsonp"];
+/******/ 	window["webpackJsonp"] = function webpackJsonpCallback(chunkIds, moreModules) {
 /******/ 		// add "moreModules" to the modules object,
 /******/ 		// then flag all "chunkIds" as loaded and fire callback
 /******/ 		var moduleId, chunkId, i = 0, callbacks = [];
@@ -18,10 +17,7 @@ var common =
 /******/ 		if(parentJsonpFunction) parentJsonpFunction(chunkIds, moreModules);
 /******/ 		while(callbacks.length)
 /******/ 			callbacks.shift().call(null, __webpack_require__);
-/******/ 		if(moreModules[0]) {
-/******/ 			installedModules[0] = 0;
-/******/ 			return __webpack_require__(0);
-/******/ 		}
+
 /******/ 	};
 
 /******/ 	// The module cache
@@ -31,7 +27,7 @@ var common =
 /******/ 	// "0" means "already loaded"
 /******/ 	// Array means "loading", array contains callbacks
 /******/ 	var installedChunks = {
-/******/ 		2:0
+/******/ 		0:0
 /******/ 	};
 
 /******/ 	// The require function
@@ -77,7 +73,7 @@ var common =
 /******/ 			script.charset = 'utf-8';
 /******/ 			script.async = true;
 
-/******/ 			script.src = __webpack_require__.p + "" + chunkId + ".js";
+/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + ({}[chunkId]||chunkId) + ".js";
 /******/ 			head.appendChild(script);
 /******/ 		}
 /******/ 	};
@@ -89,7 +85,7 @@ var common =
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/assets/";
+/******/ 	__webpack_require__.p = "/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
@@ -97,14 +93,31 @@ var common =
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	/**
-	 * Polyfills
-	 * Basic functions
-	 * Other common stuff
-	 */
-	"use strict";
+	'use strict';
+
+	__webpack_require__.e/* nsure */(1, function(require) {
+
+	  let Menu = __webpack_require__(1).default;
+	  let pandaMenu = new Menu({
+	    title: "Меню панды",
+	    items: [{
+	      text: 'Яйца',
+	      href: '#eggs'
+	    }, {
+	      text: 'Мясо',
+	      href: '#meat'
+	    }, {
+	      text: '99% еды - бамбук!',
+	      href: '#bamboo'
+	    }]
+	  });
+
+	  document.body.appendChild(pandaMenu.elem);
+
+	});
+
 
 /***/ }
 /******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,46 @@
 'use strict';
 
-const path = require('path');
-const NODE_ENV = process.env.NODE_ENV || 'development';
-const webpack = require('webpack');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-// const AssetsPlugin = require('assets-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const rimraf = require('rimraf');
-
-
-function addHash(template, hash) {
-    return NODE_ENV == 'production' ?
-        template.replace(/\.[^.]+$/, `.[${hash}]$&`) : `${template}?hash=[${hash}]`;
-}
+let webpack = require('webpack');
+let path = require('path');
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    context: path.resolve(__dirname, 'frontend'),
-    entry:   {
-        home:   './home',
-        about:  './about',
-        common: './common'
-    },
-    output:  {
-        path:           path.resolve(__dirname, 'public', 'assets'),
-        publicPath:    '/assets/',
-        filename:      addHash('[name].js', 'chunkhash'),
-        chunkFilename: addHash('[id].js', 'chunkhash'),
-        library:       '[name]'
-    },
+  context: path.resolve(__dirname, 'frontend'),
+  entry:   {
+    main: './main'
+  },
+  output:  {
+    path:       __dirname + '/public',
+    publicPath: '/',
+    filename:   '[name].js'
+  },
 
-    resolve: {
-        extensions: ['', '.js', '.styl']
-    },
+  module: {
 
-    module: {
+    loaders: [{
+      test:   /\.js$/,
+      include: __dirname + '/frontend',
+      loader: "babel?presets[]=es2015"
+    }, {
+      test:   /\.jade$/,
+      loader: "jade"
+    }, {
+      test:   /\.styl$/,
+      loader: 'style!css!stylus?resolve url'
+    }, {
+      test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+      loader: 'file?name=[name].[ext]?[hash]'
+    }]
 
-        loaders: [{
-            test:   /\.js$/,
-            loader: "babel?presets[]=es2015"
-        }, {
-            test:   /\.jade$/,
-            loader: "jade"
-        }, {
-            test:   /\.styl$/,
-            loader: ExtractTextPlugin.extract('css!stylus?resolve url')
-        }, {
-            test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
-            loader: addHash('file?name=[path][name].[ext]', 'hash:6')
-        }]
+  },
 
-    },
+  devServer: {
 
-    plugins: [
-        {
-            apply: (compiler) => {
-                rimraf.sync(compiler.options.output.path);
-            }
-        },
-        new ExtractTextPlugin(addHash('[name].css', 'contenthash'), {allChunks: true}),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common'
-        }),
-        // new AssetsPlugin({
-        //     filename: 'assets.json',
-        //     path:     path.resolve(__dirname, 'public', 'assets')
-        // })
-        new HtmlWebpackPlugin({
-            filename: './about.html',
-            chunks: ['common', 'about']
-        }),
-        new HtmlWebpackPlugin({
-            filename: './home.html',
-            chunks: ['common', 'home']
-        })
-    ]
+    host: 'localhost', // default
+    port: 8080 // default
+
+  }
 };
+
+
+
