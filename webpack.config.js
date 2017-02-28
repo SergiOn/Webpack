73dad0d23b7e44f242042c968f2be6d2d259df84
SergiOn <Onishhenko@gmail.com>
2017-02-28 22:08:32 +0200

    08-live-reload-4

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,14 @@
 'use strict';
 
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
 module.exports = {
-  context: path.resolve(__dirname, 'frontend'),
+  context: __dirname + '/frontend',
 
-  entry: {  // --inline --hot
-    main: ['webpack-dev-server/client', 'webpack/hot/dev-server', './main']
+  entry: {
+    main: './main'
   },
 
   output: {
-    path:        path.resolve(__dirname, 'public'),
+    path:       __dirname + '/public',
     publicPath: '/',
     filename:   '[name].js'
   },
@@ -21,14 +17,13 @@ module.exports = {
 
     loaders: [{
       test:    /\.js$/,
-      include:  path.resolve(__dirname, 'frontend'),
+      include: __dirname + '/frontend',
       loader:  "babel?presets[]=es2015"
     }, {
       test:   /\.jade$/,
       loader: "jade"
     }, {
       test:   /\.styl$/,
-      // loader: ExtractTextPlugin.extract('style', 'css!stylus?resolve url')
       loader: 'style!css!stylus?resolve url'
     }, {
       test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
@@ -37,16 +32,9 @@ module.exports = {
 
   },
 
-  plugins: [
-    // new ExtractTextPlugin('[name].css', {allChunks: true, disable: process.env.NODE_ENV=='development'}),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-
   devServer: {
-    contentBase:  path.resolve(__dirname, 'backend'),
+    contentBase: __dirname + '/backend',
     hot: true
   }
 };
-
-
 
